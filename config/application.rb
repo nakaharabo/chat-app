306cf7b0688f85cfb3a13a5445b97d48c548c5d0
nakaharabo <77267456+nakaharabo@users.noreply.github.com>
2021-01-21 21:04:56 +0900

    フロント実装の準備

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module ChatApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.
+    config.load_defaults 6.0
      config.generators do |g|
       g.stylesheets false
       g.javascripts false
